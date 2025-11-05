b8d580f0fc9eaa33d5f1960f226c052b0dcdba14
seenevasan77 <148300891+seenevasan77@users.noreply.github.com>
2025-11-04 21:25:54 -0800

    TNSIF _ placement project main

---
+++ PlacementRepository.java
@@ -0,0 +1,8 @@
+package placementproject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlacementRepository extends JpaRepository<Placement, Integer> {
+}
---
+++ ShajanehswarPlacementProjectApplication.java
@@ -0,0 +1,13 @@
+package placementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShajanehswarPlacementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShajanehswarPlacementProjectApplication.class, args);
+	}
+
+}
